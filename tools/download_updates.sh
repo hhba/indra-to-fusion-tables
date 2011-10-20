90e98e3ace83eecc5900f05996ff8bcaa4e83fda
Martin Sarsale <martin@sumavisos.com>
2011-10-19 23:49:26 -0300

    script to download updates

---
+++ tools/download_updates.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget --no-verbose --recursive --continue --no-directories --no-parent --directory-prefix ../data/ --accept .tar.gz http://descargas.elecciones2011.gob.ar/descargas/descargas.htm
