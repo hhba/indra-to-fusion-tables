db2e26e321eb86f910aa65cba24bef4319d41a17
Martin Sarsale <martin@sumavisos.com>
2011-10-23 21:18:36 -0300

    quote bugfix

--- tools/upload_all.rb
+++ tools/upload_all.rb
@@ -36,9 +36,9 @@ d["tablas"].each{|fn,table_id|
         csv_in.each { |row|
           if not columns
             columns = csv_in.headers
-            columns_sql = columns.map{|i| "'#{i.gsub("'","\'")}'"}.join(",")
+            columns_sql = columns.map{|i| "'#{i.gsub("'","\\'")}'"}.join(",")
           end
-          values_sql = columns.map{|column| "'#{row[column].gsub("'","\'")}'"}.join(",")
+          values_sql = columns.map{|column| "'#{row[column].gsub("'","\\'")}'"}.join(",")
           sql << "INSERT INTO #{table_id} (#{columns_sql}) values (#{values_sql});"
           if sql.length > 100000
             #puts sql
